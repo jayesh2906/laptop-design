cbf2a01c5751e32334acb10dae99fd577b0f3770
jayesh2906 <jayesh.choudhary2906@gmail.com>
2022-02-26 18:04:02 +0530

    Laptop-design-ppt

--- src/components/Laptop.js
+++ src/components/Laptop.js
@@ -60,7 +60,7 @@ const Laptop = () => {
   return (
     <div className="full-screen flex sm:flex-row xs:flex-col xs:gap-12 md:gap-24 lg:gap-40 items-center justify-center hover:cursor-pointer w-screen h-screen">
       {celebrate && <Confetti className="absolute w-full h-full" />}
-      <div className="laptop-wrapper flex flex-col  xs:w-4/5 xs:h-3/4 sm:w-1/2 sm:h-[65%] md:h-3/4 lg:h-4/5 xl:h-[95vh] xl:w-[42%]">
+      <div className="laptop-wrapper flex flex-col xs:w-4/5 xs:h-3/4 sm:w-1/2 sm:h-[65%] md:h-3/4 lg:h-4/5 xl:h-[95vh] xl:w-[42%]">
         <TopPanel
           power={power}
           fingerScan={fingerScan}
